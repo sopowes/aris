b09dfcb1bfe4f2d18992c27f448e354c516d4344
sopowes <67716619+sopowes@users.noreply.github.com>
2020-07-02 08:37:22 +0700

    Update au.php

--- au.php
+++ au.php
@@ -38,3 +38,62 @@ ulang:
         $ otptoken = getStr ( '"otp_token": "' , '"' , $ register );
          warna gema ( "putih" , "KODE OTP .." ). "\ n" ;
         otp:
+         warna gema ( "putih" , "Otp:" );
+        $ otp = trim ( fgets ( STDIN ));
+        $ data1 = '{"client_name": "gojek: cons: android", "data": {"otp": "' . $ otp . '", "otp_token": "' . $ otptoken . '"}, " client_secret ":" 83415d06-ec4e-11e6-a41b-6c40088ab51e "} ' ;
+        $ verif = permintaan ( "/ v5 / pelanggan / telepon / verifikasi" , null , $ data1 );
+        if ( strpos ( $ verif , '"access_token"' )) {
+         warna gema ( "putih" , "BERHASIL MENDA berlangganan \ n" );
+        $ token = getStr ( '"access_token": "' , '"' , $ verif );
+        $ uuid = getStr ( '"resource_owner_id":' , ',' , $ verif );
+         warna gema ( "putih" , "+] Token akses Anda:" . $ token . "\ n \ n" );
+        save ( "token.txt" , $ token );
+         warna gema ( "putih" , "\ n▬▬▬▬▬▬▬▬▬▬▬▬CLAIM VOUCHER▬▬▬▬▬▬▬▬▬▬▬▬" );
+        gema  "\ n" . warna ( "putih" , "KLAIM A .." );
+        gema  "\ n" . warna ( "putih" , "Harap tunggu" );
+        untuk ( $ a = 1 ; $ a <= 3 ; $ a ++) {
+         warna gema ( "putih" , "." );
+        tidur ( 5 );
+        }
+        $ code1 = permintaan ( '/ go-promotion / v1 / promotion / pendaftaran' , $ token , '{"promo_code": "EATLAH"}' );
+        $ message = fetch_value ( $ code1 , '"message": "' , '"' );
+        if ( strpos ( $ code1 , 'Promo kamu sudah bisa dipakai' )) {
+        gema  "\ n" . warna ( "hijau" , "Pesan:" . $ pesan );
+        goto gocar;
+        } lain {
+        gema  "\ n" . warna ( "putih" , "Pesan:" . $ pesan );
+	gocar:
+        gema  "\ n" . warna ( "putih" , "KLAIM B .." );
+        gema  "\ n" . warna ( "putih" , "Harap tunggu" );
+        untuk ( $ a = 1 ; $ a <= 3 ; $ a ++) {
+         warna gema ( "putih" , "." );
+        tidur ( 35 );
+        }
+        $ code1 = permintaan ( '/ promosi-pergi / v1 / promosi / pendaftaran' , $ token , '{"promo_code": "COBAGOFOOD2206"}' );
+        $ message = fetch_value ( $ code1 , '"message": "' , '"' );
+        if ( strpos ( $ code1 , 'Promo kamu sudah bisa dipakai.' )) {
+        gema  "\ n" . warna ( "hijau" , "Pesan:" . $ pesan );
+        goto gofood;
+        } lain {
+        gema  "\ n" . warna ( "putih" , "Pesan:" . $ pesan );
+        gofood:
+        gema  "\ n" . warna ( "putih" , "KLAIM C .." );
+        gema  "\ n" . warna ( "putih" , "Harap tunggu" );
+        untuk ( $ a = 1 ; $ a <= 3 ; $ a ++) {
+         warna gema ( "putih" , "." );
+        tidur ( 3 );
+        }
+        $ code1 = permintaan ( '/ promosi-pergi / v1 / promosi / pendaftaran' , $ token , '{"promo_code": "PESANGOFOOD2206"}' );
+        $ message = fetch_value ( $ code1 , '"message": "' , '"' );
+        gema  "\ n" . warna ( "putih" , "Pesan:" . $ pesan );
+        gema  "\ n" . warna ( "putih" , "VOCER COLI." );
+        gema  "\ n" . warna ( "putih" , "Harap tunggu" );
+        untuk ( $ a = 1 ; $ a <= 3 ; $ a ++) {
+         warna gema ( "putih" , "." );
+        tidur ( 3 );
+        }
+        tidur ( 3 );
+        $ boba09 = permintaan ( '/ promosi-promosi / v1 / promosi / pendaftaran' , $ token , '{"promo_code": "PAKEGOFOOD0906"}' );
+        $ messageboba09 = fetch_value ( $ boba09 , '"message": "' , '"' );
+        gema  "\ n" . warna ( "putih" , "Pesan:" . $ messageboba09 );
+        tidur ( 1 );
